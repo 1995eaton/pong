ce9ccbd70aabf77e9d45a77a8cbcb150aee5527f
1995eaton <1995eaton@gmail.com>
2014-02-22 15:04:52 +0000

    a;lsebmase;aisgit add *

--- main.js
+++ main.js
@@ -1,301 +1,384 @@
-var addPlayers, addBall, randRange, gameStart, updateScore, addScores;
-var player1, player1s, y1, player2, player2s, y2;
-var upPressed, downPressed;
-
-var ball, ballX, ballY, reset, cDiv, continuePrompt;
-
+var player, playerScore, cpu, cpuScore, vx, vy, ball, ballX, ballY, cDiv, continuePrompt, upPressed, downPressed, y1, y2, isTouchDevice;
+var gameOver = false;
+var paddleHeight = 130;
+var ballHeight = 24;
 var p1Lost = false;
-var p2Lost = false;
-
-var keySpeed = 4;
-
 var gameStarted = false;
 
-var winningScore = 5;
-
-randRange = function(min, max) {
-  return Math.floor((Math.random()*max)+min);
+var randRange = function(min, max) {
+    return Math.floor((Math.random()*max)+min);
 }
 
-reset = {
-  ball: function() {
-    ballX = window.innerWidth/2-12;
-    ballY = window.innerHeight/2-12;
-    ball.style.top = ballY+"px";
-    ball.style.left = ballX+"px";
-    gameStarted = false;
-  },
-  scores: function() {
-    player1s.style.left = "90px";
-    player1s.style.top = "100px";
-    player1s.innerHTML = "0";
-    player2s.style.right = "90px";
-    player2s.style.top = "100px";
-    player2s.innerHTML = "0";
-  },
+var reset = {
+    ball: function() {
+        ballX = window.innerWidth/2-12;
+        ballY = window.innerHeight/2-12;
+        ball.style.top = ballY+"px";
+        ball.style.left = ballX+"px";
+        gameStarted = false;
+    },
+    scores: function() {
+        playerScore.style.left = "90px";
+        playerScore.style.top = "100px";
+        playerScore.innerHTML = "0";
+        cpuScore.style.right = "90px";
+        cpuScore.style.top = "100px";
+        cpuScore.innerHTML = "0";
+    },
 };
 
-updateScore = function(winner) {
+var updateScore = function(winner) {
 
-  if (winner === "Player 1" && player1s.innerHTML < winningScore) {
-    player1s.innerHTML++;
-    cDiv.innerHTML = winner+" scored!<br/><br/>Press space to continue";
-    continuePrompt(true);
-  } else if (winner === "Player 2" && player2s.innerHTML < winningScore) {
-    player2s.innerHTML++;
-    cDiv.innerHTML = winner+" scored!<br/><br/>Press space to continue";
-    continuePrompt(true);
-  }
-
-  reset.ball();
-  if (parseInt(player1s.innerHTML) == winningScore) {
-    player2s.innerHTML = "GAME OVER!";
-  } else if (parseInt(player2s.innerHTML) == winningScore) {
-    player1s.innerHTML = "GAME OVER!";
-  } else { return; }
-
-  cDiv.innerHTML = winner+" won the game!<br/><br/>Press space to play again";
-  gameStarted = false;
-
-  var waitForClick = setInterval(function() {
-    if (gameStarted) {
-      reset.scores();
-      clearInterval(waitForClick);
+    var winningScore = 5;
+    var deviceGesture = (isTouchDevice)? "Tap to continue" : "Press space to continue";
+    if (winner === "Player 1" && playerScore.innerHTML < winningScore) {
+        playerScore.innerHTML++;
+        cDiv.innerHTML = winner+" scored!<br/><br/>"+deviceGesture;
+        continuePrompt(true);
+    } else if (winner === "CPU" && cpuScore.innerHTML < winningScore) {
+        cpuScore.innerHTML++;
+        cDiv.innerHTML = winner+" scored!<br/><br/>"+deviceGesture;
+        continuePrompt(true);
     }
-  }, 200);
+
+    reset.ball();
+    if (parseInt(playerScore.innerHTML) == winningScore) {
+        cpuScore.innerHTML = "GAME OVER!";
+    } else if (parseInt(cpuScore.innerHTML) == winningScore) {
+        playerScore.innerHTML = "GAME OVER!";
+    } else { return; }
+
+    cDiv.innerHTML = winner+" won the game!<br/><br/>Press space to play again";
+    continuePrompt(true);
+    gameOver = true;
+    gameStarted = false;
 
 }
 
-continuePrompt = function(state) {
+var continuePrompt = function(state) {
 
-  if (state) {
-    cDiv.style.display = "block";
-  } else {
-    cDiv.style.display = "none";
-  }
+    if (state) {
+        cDiv.style.display = "block";
+    } else {
+        cDiv.style.display = "none";
+    }
 
 }
 
-moveBall = function() {
-  var vx, vy, direction, angle, accel;
-  var playerHasWon = false;
-  var difficultyFactor = 1.1;
-  var maxVeloctiy = 7;
-  if (p1Lost) { direction = "right"; }
-  else { direction = "left"; }
-  vx = 1; // initial x velocity
-  var initialDir = (randRange(0, 100)%10 >= 5)? -1 : 1; // should the ball go up or down?
-  vy = randRange(0.75*100, (vx+1)*100)/100*initialDir; // definitely not horizonal!
+var moveCpuToMiddle = function() {
 
-  var initMove = setInterval(function() {
-
-    ballX += ((direction==="left")? -1 : 1) * vx;
-    ballY += ((direction==="left")? -1 : 1) * vy;
-    ball.style.left = ballX+"px";
-    ball.style.top = ballY+"px";
-
-    if (ballY <= 0) {
-      vy = -1*vy;
-    } else if (ballY >= window.innerHeight-24) {
-      vy = -1*vy;
+    if (y2+paddleHeight-ballHeight === ballY) return;
+    if (y2+paddleHeight-ballHeight > ballY) {
+        var goUp = setInterval(function() {
+            y2 -= 1;
+            cpu.style.top = y2+"px";
+            if (y2+63 <= ballY) {
+                clearInterval(goUp);
+            }
+        }, 4)
+    } else {
+        var goDown = setInterval(function() {
+            y2 += 1;
+            cpu.style.top = y2+"px";
+            if (y2+63 >= ballY) {
+                clearInterval(goDown);
+            }
+        }, 4);
     }
 
-    if (ballX <= 24) {
-      if (y1 <= ballY+24 && y1 >= ballY-130) {
-        console.log("p1 -> hit");
-        if (vy < maxVeloctiy) {
-          vy = -1*vy*difficultyFactor;
+}
+
+var cpuHit;
+var computer = function() {
+    var above;
+    var speed;
+    if (isTouchDevice) {
+        speed = 0.0035 * (window.innerWidth);
+    } else {
+        speed = 0.85;
+    }
+    var moveCPU = setInterval(function() {
+        above = y2 > ballY - paddleHeight/2 + ballHeight/2;
+        if (above && y2 >= 0) {
+            if (!cpuHit) {
+                y2 -= speed*Math.abs(vy)*Math.abs(ballX/window.innerWidth) + 1;
+            } else {
+                y2 -= speed*Math.abs(vy)*Math.abs(ballX/window.innerWidth)*0.3;
+            }
+            cpu.style.top = y2+"px";
+        } else if (y2 <= window.innerHeight-paddleHeight) {
+            if (!cpuHit) {
+                y2 += speed*Math.abs(vy)*Math.abs(ballX/window.innerWidth) + 1;
+            } else {
+                y2 += speed*Math.abs(vy)*Math.abs(ballX/window.innerWidth)*0.3;
+            }
+            cpu.style.top = y2+"px";
         }
-        if (vx < maxVeloctiy) {
-          vx *= difficultyFactor;
+        if (!gameStarted) {
+            moveCpuToMiddle();
+            clearInterval(moveCPU);
         }
+    }, 4);
+}
+
+var moveBall = function() {
+    if (gameOver) {
+        gameOver = false;
+        reset.scores();
+    }
+    computer();
+    var direction;
+    var difficultyFactor = 1.1;
+    var maxXVelocity, maxYVelocity;
+    if (isTouchDevice) {
+        maxXVelocity = 5;
+        maxYVelocity = 2;
+    } else {
+        maxXVelocity = 5;
+        maxYVelocity = 3;
+    }
+    var delay = false;
+    if (p1Lost) {
         direction = "right";
-      } else if (ballX <= 0) {
-        updateScore("Player 2");
-        p1Lost = true;
-        p2Lost = false;
-        clearInterval(initMove);
-      }
-    }
-    
-    else if (ballX >= window.innerWidth-48) {
-      if (y2 <= ballY+12 && y2 >= ballY-130) {
-        console.log("p2 -> hit");
-        if (vy < maxVeloctiy) {
-          vy = -1*vy*difficultyFactor;
-        }
-        if (vx < maxVeloctiy) {
-          vx *= difficultyFactor;
-        }
+    } else {
         direction = "left";
-      } else if (ballX >= window.innerWidth){
-        updateScore("Player 1");
-        p1Lost = false;
-        p2Lost = true;
-        clearInterval(initMove);
-      }
+    }
+    var initialDir = (randRange(0, 100)%10 >= 5)? -1 : 1;
+    vx = 2; // initial x velocity
+    vy = randRange(20, 60)/100*initialDir;
+    var up = upPressed;
+    var spin = 0;
+    var spinFactor = 0.05;
+
+    var initMove = setInterval(function() {
+
+        if (upPressed) {
+            if (!up || y1 <= 0) {
+                spin = 0;
+            }
+            up = true;
+            spin += spinFactor;
+
+        } else if (downPressed) {
+            if (up || y1 >= window.innerHeight-paddleHeight) {
+                spin = 0;
+            }
+            up = false;
+            spin -= spinFactor;
+        } else {
+            spin = 0;
+        }
+
+        if (!delay) {
+            if (ballY <= 0 || ballY >= window.innerHeight-ballHeight) {
+                delay = true;
+                setTimeout(function() { // keep the ball from being flipped twice
+                    delay = false;
+                }, 50);
+                if (ballY <= 0) {
+                    vy = -1*vy;
+                } else if (ballY >= window.innerHeight-ballHeight) {
+                    vy = -1*vy;
+                }
+            }
+        }
+
+        ballX += ((direction==="left")? -1 : 1) * vx;
+        ball.style.left = ballX+"px";
+        ballY += ((direction==="left")? -1 : 1) * vy;
+        if (ballY < 0) {
+            ballY = 0;
+        } else if (ballY > window.innerHeight-ballHeight) {
+            ballY = window.innerHeight-ballHeight;
+        }
+        ball.style.top = ballY+"px";
+
+        if (ballX <= ballHeight) {
+            cpuHit = false;
+            if (y1 <= ballY+ballHeight && y1 >= ballY-paddleHeight) {
+                if (vy < maxYVelocity) {
+                    vy = -1*vy*difficultyFactor+spin;
+                    spin = 0;
+                } else {
+                    vy = -1*vy;
+                    spin = 0;
+                }
+                if (vx < maxXVelocity) {
+                    vx *= difficultyFactor;
+                }
+                direction = "right";
+            } else if (ballX <= 0) {
+                updateScore("CPU"); // player 2 wins
+                p1Lost = true;
+                clearInterval(initMove);
+            }
+        }
+
+        else if (ballX >= window.innerWidth-ballHeight*2) {
+            cpuHit = true;
+            if (y2 <= ballY+ballHeight && y2 >= ballY-paddleHeight) {
+                if (vy < maxYVelocity) {
+                    vy *= -1*difficultyFactor;
+                } else {
+                    vy *= -1;
+                }
+                if (vx < maxXVelocity) {
+                    vx *= difficultyFactor;
+                }
+                direction = "left";
+            } else if (ballX >= window.innerWidth-ballHeight){
+                updateScore("Player 1"); // player 1 wins
+                p1Lost = false;
+                clearInterval(initMove);
+            }
+        }
+
+    }, 4);
+
+}
+
+var create  = {
+
+    playerScore: function() {
+        player = document.createElement("div");
+        player.id = "paddle";
+        player.style.left = "0";
+        player.style.top = window.innerHeight/2-paddleHeight/2+"px";
+        cpu = document.createElement("div");
+        cpu.id = "paddle";
+        cpu.style.right = "0";
+        cpu.style.top = window.innerHeight/2-paddleHeight/2+"px";
+        if (isTouchDevice) {
+            paddleHeight = 80;
+            cpu.style.height = player.style.height = paddleHeight+"px";
+        }
+        y1 = y2 = window.innerHeight/2-paddleHeight/2;
+
+            document.body.insertBefore(player, document.body.firstChild);
+        document.body.insertBefore(cpu, document.body.firstChild);
+    },
+    scores: function() {
+        playerScore = document.createElement("div");
+        playerScore.id = "score";
+        playerScore.style.left = "90px";
+        playerScore.style.top = "100px";
+        playerScore.innerHTML = "0";
+        cpuScore = document.createElement("div");
+        cpuScore.id = "score";
+        cpuScore.style.right = "90px";
+        cpuScore.style.top = "100px";
+        cpuScore.innerHTML = "0";
+        document.body.insertBefore(playerScore, document.body.firstChild);
+        document.body.insertBefore(cpuScore, document.body.firstChild);
+    },
+    ball: function() {
+        ball = document.createElement("div");
+        ball.id = "ball";
+        ballX = window.innerWidth/2-ballHeight/2; //12 -> ball-width
+        ballY = window.innerHeight/2-ballHeight/2;
+        ball.style.top = ballY+"px";
+        ball.style.left = ballX+"px";
+        document.body.insertBefore(ball, document.body.firstChild);
+    },
+    mainText: function() {
+        cDiv = document.createElement("div");
+        cDiv.id = "pressSpaceBox";
+        cDiv.style.top = window.innerHeight/2-200+"px";
+        cDiv.style.left = window.innerWidth/2-400+"px";
+        if (isTouchDevice) {
+            cDiv.style.fontSize = "11pt";
+            cDiv.innerHTML = "PONG<br/><br/>Tap to play!";
+        } else {
+            cDiv.innerHTML = "PONG<br/><br/>press SPACE to play!";
+        }
+        document.body.insertBefore(cDiv, document.body.firstChild);
     }
 
-  }, 0);
 }
 
-addPlayers = function() {
+var gameStart = function() {
 
-  player1 = document.createElement("div");
-  player1.id = "paddle";
-  player1.style.left = "0";
-  player1.style.top = window.innerHeight/2-75+"px"
-  player2 = document.createElement("div");
-  player2.id = "paddle";
-  player2.style.right = "0";
-  player2.style.top = window.innerHeight/2-75+"px"
-
-  document.body.insertBefore(player1, document.body.firstChild);
-  document.body.insertBefore(player2, document.body.firstChild);
+    create.playerScore();
+    create.ball();
+    create.scores();
+    create.mainText();
+    continuePrompt(true);
 
 }
 
-addScores = function() {
+var keyMovement = function() {
 
-  player1s = document.createElement("div");
-  player1s.id = "score";
-  player1s.style.left = "90px";
-  player1s.style.top = "100px";
-  player1s.innerHTML = "0";
-  player2s = document.createElement("div");
-  player2s.id = "score";
-  player2s.style.right = "90px";
-  player2s.style.top = "100px";
-  player2s.innerHTML = "0";
-  document.body.insertBefore(player1s, document.body.firstChild);
-  document.body.insertBefore(player2s, document.body.firstChild);
-
-}
-
-addBall = function() {
-
-  ball = document.createElement("div");
-  ball.id = "ball";
-  ballX = window.innerWidth/2-12; //12 -> ball-width
-  ballY = window.innerHeight/2-12;
-  ball.style.top = ballY+"px";
-  ball.style.left = ballX+"px";
-  document.body.insertBefore(ball, document.body.firstChild);
+    var keySpeed = 5;
+    setInterval(function() {
+        if (upPressed) {
+            if (y1 >= 0) {
+                y1 -= keySpeed;
+            }
+        }
+        if (downPressed) {
+            if (y1 <= window.innerHeight-paddleHeight) {
+                y1 += keySpeed;
+            }
+        }
+        player.style.top = y1+"px";
+    }, 4);
 
 }
 
-addContinueDiv = function() {
+var onKeyUp = function(e) {
 
-  cDiv = document.createElement("div");
-  cDiv.id = "pressSpaceBox";
-  cDiv.style.top = window.innerHeight/2-200+"px";
-  cDiv.style.left = window.innerWidth/2-400+"px";
-  cDiv.innerHTML = "PONG<br/><br/>press SPACE to play";
-  document.body.insertBefore(cDiv, document.body.firstChild);
-
-}
-
-gameStart = function() {
-
-  addPlayers();
-  addBall();
-  addScores();
-  addContinueDiv();
-  continuePrompt(true);
-
-}
-
-
-onMouseMove = function(e) {
-  y1 = e.y-75;
-  if (y1 <= 0) { y1 = 0; }
-  if (y1 >= window.innerHeight-130) { y1 = window.innerHeight-130; }
-  y2 = e.y-75;
-  if (y2 <= 0) { y2 = 0; }
-  if (y2 >= window.innerHeight-130) { y2 = window.innerHeight-130; }
-  player1.style.top = y1+"px"; //75 -> paddle-height
-  player2.style.top = y2+"px";
-}
-
-keyMovement = function() {
-  setInterval(function() {
-    if (upPressed) {
-      if (y1 >= 0) {
-        y1 -= keySpeed;
-        player1.style.top = y1+"px"; //75 -> paddle-height
-      }
-      if (y2 >= 0) {
-        y2 -= keySpeed;
-      }
+    switch (e.which) {
+        case 38: case 75:
+            upPressed = false;
+        case 40: case 74:
+            downPressed = false;
     }
-    if (downPressed) {
-      if (y1 <= window.innerHeight-130) {
-        y1 += keySpeed;
-      }
-      if (y2 <= window.innerHeight-130) {
-        y2 += keySpeed;
-      }
+
+}
+
+var onKeyPress = function(e) {
+
+    switch (e.which) {
+        case 38: case 75: // up arrow + k
+            upPressed = true;
+            break;
+        case 40: case 74: // down arrow + j
+            downPressed = true;
+            break;
     }
-    player1.style.top = y1+"px"; //75 -> paddle-height
-    player2.style.top = y2+"px";
-  }, 0);
-}
-
-onKeyUp = function(e) {
-  switch (e.which) {
-    case 38:
-      upPressed = false;
-    case 40:
-      downPressed = false;
-    default:
-      break;
-  }
-}
-
-onKeyPress = function(e) { // let space be used to continue
-
-  console.log(e.which);
-  switch (e.which) {
-    case 32:
-      if (!gameStarted) {
+    if (e.which == 32 && !gameStarted) {
         gameStarted = true;
         continuePrompt(false);
         moveBall();
-        break;
-      }
-    case 38: // up arrow
-      upPressed = true;
-      break;
-    case 40:
-      downPressed = true;
-      break;
-    default:
-      break;
-  }
-  if (e.which == 32 && !gameStarted) {
-    gameStarted = true;
-    continuePrompt(false);
-    moveBall();
-  }
+    }
 
 }
 
-onMouseDown = function(e) { // also allow click to continue
+var mobileTouch = function(e) {
+    e.preventDefault();
+    if (!gameStarted) {
+        gameStarted = true;
+        continuePrompt(false);
+        moveBall();
+    }
+}
 
-  if (!gameStarted) {
-    gameStarted = true;
-    continuePrompt(false);
-    moveBall();
-  }
-
+var mobileTouchMove = function(e) {
+    e.preventDefault();
+    y1 = e.pageY-paddleHeight/2;
+    player.style.top = y1+"px";
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  document.addEventListener("keydown", onKeyPress, false);
-  document.addEventListener("keyup", onKeyUp, false);
-  document.addEventListener("mousemove", onMouseMove, false);
-  document.addEventListener("mousedown", onMouseDown, false);
-  document.getElementById("center").style.left = window.innerWidth/2-3+"px"; //3 -> center-width
-  keyMovement();
-  gameStart();
+    isTouchDevice = ('ontouchstart' in window || 'onmsgesturechange' in window);
+    if (isTouchDevice) {
+        document.addEventListener("touchstart", mobileTouch, false);
+        document.addEventListener("touchmove", mobileTouchMove, false);
+    } else {
+        document.addEventListener("keydown", onKeyPress, false);
+        document.addEventListener("keyup", onKeyUp, false);
+        keyMovement();
+    }
+    document.getElementById("center").style.left = window.innerWidth/2-5+"px";
+    gameStart();
 });
